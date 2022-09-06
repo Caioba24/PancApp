a92e2a5d16b571755ac8091b7bda58fd9ae9ea7e
Caioba24 <dipietroapg@gmail.com>
2022-09-06 20:54:56 -0300

    commit de precaucao

---
+++ app/src/main/java/br/com/etecia/pancapp/TabLayout.java
@@ -0,0 +1,14 @@
+package br.com.etecia.pancapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TabLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_activity);
+    }
+}
